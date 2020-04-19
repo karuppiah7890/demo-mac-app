cdae6c5c050dbace32cd5f4c93194fa5f4f86255
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-04-19 11:16:19 +0530

    Initial Commit

---
+++ demo-mac-app/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  demo-mac-app
+//
+//  Created by Karuppiah N on 4/19/20.
+//  Copyright © 2020 Karuppiah Natarajan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
